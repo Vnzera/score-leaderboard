2d0c8ec5e857e9a3eda2cc1a1e2d004a292ea386
Luis Diaz <luis@linuxmail.org>
2019-09-21 01:44:22 -0700

    Configure Passport and Jwt modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    // config for the nestjs passport module
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // config for the nestjs jwt module
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
     // make UserRepository available for injection throughout this Auth module
     TypeOrmModule.forFeature([UserRepository]),
   ],
